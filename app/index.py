6a552eb3007683261fe19d0b7211c6204fe31d28
IgorL123 <Zedenbraucht@inbox.ru>
2024-06-24 17:44:02 +0300

    formatting

--- app/index.py
+++ app/index.py
@@ -1,5 +1,11 @@
 import ngtpy
 import clickhouse_connect
+import os
+
+host = os.environ["CLICKHOUSE_HOST"]
+user = os.environ["CLICKHOUSE_USER"]
+pasw = os.environ["CLICKHOUSE_PASSWORD"]
+
 
 DIM = 768
 
@@ -18,14 +24,15 @@ def get_top(emb, top: int = 100):
 
 
 def update_index():
-    client = clickhouse_connect.get_client(host="mutagen.space", username="default",
-                                           password="iyLOLr1EXIWin8z4zMNe3UFKJ5qoba")
+    client = clickhouse_connect.get_client(host=host, username=user, password=pasw)
     uuids = []
 
     ngtpy.create(b"storage", DIM, distance_type="Cosine")
     index = ngtpy.Index(b"storage", zero_based_numbering=True)
 
-    with client.query_row_block_stream(f"SELECT * from vr.embeddings") as stream:
+    with client.query_row_block_stream(
+        f"SELECT * from vr.embeddings limit 1000000"
+    ) as stream:
         batch = []
         for block in stream:
             for row in block:
